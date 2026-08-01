2b0991e07c92343e3d76da291c27fd6324c1bc63
rahul4work <heyyrj98@gmail.com>
2026-08-02 00:24:46 +0530

    fixed google logo

--- Frontend/src/features/auth/components/ContinueWithGoogle.jsx
+++ Frontend/src/features/auth/components/ContinueWithGoogle.jsx
@@ -1,3 +1,5 @@
+import googleLogo from "../../../assets/google.svg";
+
 const ContinueWithGoogle = ({ onClick }) => {
   return (
     <button
@@ -5,11 +7,7 @@ const ContinueWithGoogle = ({ onClick }) => {
       onClick={onClick}
       className="w-full h-12 border border-gray-300 rounded-xl bg-white hover:bg-gray-50 flex items-center justify-center gap-3 transition cursor-pointer"
     >
-      <img
-        src="https://www.svgrepo.com/show/475656/google-color.svg"
-        alt="google"
-        className="w-5 h-5"
-      />
+      <img src={googleLogo} alt="Google" className="w-5 h-5" />
       <span className="font-medium text-gray-700">Continue with Google</span>
     </button>
   );
