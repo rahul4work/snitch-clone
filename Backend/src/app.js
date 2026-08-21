a7c57539e48993a17f7d45bf8a8487cb42ce5018
rahul4work <heyyrj98@gmail.com>
2026-08-21 22:38:47 +0530

    updated callbackURL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -40,7 +40,7 @@ passport.use(
     {
       clientID: config.GOOGLE_CLIENT_ID,
       clientSecret: config.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${config.FRONTEND_URL}/api/auth/google/callback`,
+      callbackURL: `${config.BACKEND_URL}/api/auth/google/callback`,
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
