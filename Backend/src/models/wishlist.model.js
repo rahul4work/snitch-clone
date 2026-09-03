4d170816b6456cdf1ea6b7f17be44a39f5f921cf
rahul4work <heyyrj98@gmail.com>
2026-09-03 23:59:20 +0530

    creating wishlist feature

---
+++ Backend/src/models/wishlist.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+      unique: true,
+    },
+
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "products",
+          required: true,
+        },
+        addedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const wishlistModel = mongoose.model("wishlists", wishlistSchema);
+
+export default wishlistModel;
